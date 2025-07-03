e4881e42852ed528a3e3b6995bfdb50ce106d0f8
Tomatofu <tomatofu@example.com>
2025-07-03 11:01:39 -0700

    format main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import secrets
 import string
-possible_chars = [string.punctuation, string.ascii_uppercase, string.ascii_lowercase, string.digits] # Different characters that can be used
-config = [input('What length would you like your password(s) to be? (integer)\n'), input('How may passwords would you like to generate? (integer)\n'), input('Would you like to allow special characters? (1/0)\n'), input('Would you like to allow capital letters? (1/0)\n'), input('Would you like to allow lowercase letters? (1/0)\n'), input('Would you like to allow numbers? (1/0)\n')]
+
+
+possible_chars = [
+    string.punctuation,
+    string.ascii_uppercase,
+    string.ascii_lowercase,
+    string.digits,
+]
+config = [
+    input('What length would you like your password(s) to be? (integer)\n'),
+    input('How may passwords would you like to generate? (integer)\n'),
+    input('Would you like to allow special characters? (1/0)\n'),
+    input('Would you like to allow capital letters? (1/0)\n'),
+    input('Would you like to allow lowercase letters? (1/0)\n'),
+    input('Would you like to allow numbers? (1/0)\n'),
+]
+
+
 try:
-    possible_chars = list(filter(lambda x: int(config[2:][possible_chars.index(x)]), possible_chars)) # Filters out options from the list that the user doesn't want.
+    def func(x): return int(config[2:][possible_chars.index(x)])
+    possible_chars = list(filter(func, possible_chars))
     for i in range(int(config[1])):
         password = ''
         for i in range(int(config[0])):
@@ -11,3 +28,4 @@ try:
         print(f'\n{password}')
 except:
     print('\nSomething went wrong. You may want to choose different settings.')
+
